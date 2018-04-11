43068171d9f7b51f30e31ced59063057f56567e9
Dom <d.celiano@yahoo.com>
2018-04-11 13:33:58 +0100

    Added accelerated benchmarking script.

--- unix/run_encryption_unaccel.sh
+++ unix/run_encryption_unaccel.sh
@@ -8,14 +8,15 @@ INPUT=plaintext
 OUTPUT=output.txt
 ITERS=5
 
-> output_666_0.csv #make an empty file
-> output_333_0.csv #make an empty file
-> output_166_0.csv #make an empty file
+#make empty files
+> output_666_0.csv
+> output_333_0.csv
+> output_166_0.csv
 for ((i = 0; i < $ITERS; ++i)) do
   devmem2 0xF8000120 w 0x1F000200 #change the CPU frequency to 666MHz
-  ./aes $KEY $IV $INPUT $OUTPUT >> output_666_0.csv #append measurements to the file
+  ./aes $KEY $IV $INPUT $OUTPUT >> output_666_0.csv
   devmem2 0xF8000120 w 0x1F000400 #change the CPU frequency to 333MHz
-  ./aes $KEY $IV $INPUT $OUTPUT >> output_333_0.csv #append measurements to the file
+  ./aes $KEY $IV $INPUT $OUTPUT >> output_333_0.csv
   devmem2 0xF8000120 w 0x1F000800 #change the CPU frequency to 166MHz
-  ./aes $KEY $IV $INPUT $OUTPUT >> output_166_0.csv #append measurements to the file
+  ./aes $KEY $IV $INPUT $OUTPUT >> output_166_0.csv
 done
